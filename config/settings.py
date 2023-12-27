d0c232fa3b8c4cc3e01eb48f0b11eeb9ce401f7a
kenzi1111 <88967765+kenzi1111@users.noreply.github.com>
2023-12-28 07:14:13 +0900

    既読機能修正中

--- config/settings.py
+++ config/settings.py
@@ -123,16 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-# 追加コード
-# STATIC_ROOT = "/static/"
-
-# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
