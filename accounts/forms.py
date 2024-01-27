394d83e18a9edd6f76f9973ddd1aa1f38956dff7
kenzi1111 <88967765+kenzi1111@users.noreply.github.com>
2024-01-27 15:45:56 +0900

    emailプルダウンリスト追加

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import User, UserProfile
+from django.contrib.auth import get_user_model
 
 
 # ユーザ情報のCRUD機能
@@ -10,6 +11,16 @@ class UserProfileForm(forms.ModelForm):
 
 
 class UserForm(forms.ModelForm):
+    email = forms.ChoiceField(choices=[])  # 初期状態では空の選択肢
+
     class Meta:
         model = User
-        fields = ["username", "email"]  # User モデルの username,email を扱う
+        fields = ["username", "email"]
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        # UserProfile に紐付いているユーザのEmailリストを取得
+        used_emails = UserProfile.objects.values_list('user__email', flat=True)
+        # UserProfile に紐付いていないユーザのEmailだけを取得
+        available_emails = get_user_model().objects.exclude(email__in=used_emails)
+        self.fields['email'].choices = [(user.email, user.email) for user in available_emails]
